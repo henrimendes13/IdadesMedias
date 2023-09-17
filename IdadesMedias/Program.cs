fe123820e30b632a310dfc2d2ccd9263e764a718
Henrique Mendes <henriquemendes13@gmail.com>
2023-09-16 23:51:24 -0300

    Add project files.

---
+++ IdadesMedias/Program.cs
@@ -0,0 +1,17 @@
+﻿double idade, media, qtd, soma;
+
+idade = double.Parse(Console.ReadLine());
+qtd = 0;
+soma = 0;
+
+
+while (idade >= 0)
+{
+    qtd = qtd + 1;
+    soma = soma + idade;
+    
+    idade = double.Parse(Console.ReadLine());
+}
+
+media = soma / qtd;
+Console.WriteLine($"{media}");
